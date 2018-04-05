da62eb7e11fdb705754e0308c15c20f2dea2ecde
Daniel Zheng <dzheng256@gmail.com>
2018-04-05 18:59:56 -0400

    update lextools

--- lextools/lextools.py
+++ lextools/lextools.py
@@ -10,7 +10,8 @@ __author__ = 'ELI Data Mining Group'
 import pickle
 import re
 
-LOOKUP = pickle.load('lemmatizer.pkl')
+with open('lemmatizer.pkl', 'rb') as f:
+    LOOKUP = pickle.load(f)
 def lemmatize(tokens):
     """ Lemmatize with lookup table and return list of corresponding lemmas """
     return [LOOKUP.get(x, x) for x in tokens]
@@ -22,3 +23,14 @@ def re_tokenize(text):
     Lowercase input, removing symbols and digts.
     """
     return re.findall(r"[A-Za-z]+", text.lower())
+
+
+def adv_guiraud(freq_list='NGSL', custom_list=None):
+    """
+    Calculates advanced guiraud: sqrt(advanced types / number of tokens)
+    By default, uses NGSL top 2k words as frequency list
+    custom_list is a custom list of common types for frequency list
+    """
+
+    if custom_list is not None:
+        common_types = list(set(custom_list))
