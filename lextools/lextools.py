5fc15e2f583353b5de6e6fd8ead3f1f6c5982d07
Daniel Zheng <dzheng256@gmail.com>
2018-04-05 12:05:23 -0400

    using pickle

--- lextools/lextools.py
+++ lextools/lextools.py
@@ -1,14 +1,5 @@
 """
-going to make a package similar to elitools but for lexical processing called lextools:
-MTLD
-HDD
-AG
-NGSL, PSL3, PET, Custom
-re_tokenize
-lemmatize
-bandify
-6-tuple (in, adv, misspelled)
-NGSL, PSL3, PET
+docs
 
 """
 
@@ -16,9 +7,10 @@ NGSL, PSL3, PET
 __version__ = '0.1'
 __author__ = 'ELI Data Mining Group'
 
-from spacy.lang.en import LOOKUP
 import re
+import pickle
 
+LOOKUP = pickle.load('lemmatizer.pkl')
 def lemmatize(tokens):
     """ Lemmatize with lookup table and return list of corresponding lemmas """
     return [LOOKUP.get(x, x) for x in tokens]
