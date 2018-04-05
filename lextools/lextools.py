f7d9fc82b7f6155415b51495240a904fb4680cfc
Daniel Zheng <dzheng256@gmail.com>
2018-04-05 18:45:09 -0400

    add re tokenize

--- lextools/lextools.py
+++ lextools/lextools.py
@@ -7,10 +7,18 @@ docs
 __version__ = '0.1'
 __author__ = 'ELI Data Mining Group'
 
-import re
 import pickle
+import re
 
 LOOKUP = pickle.load('lemmatizer.pkl')
 def lemmatize(tokens):
     """ Lemmatize with lookup table and return list of corresponding lemmas """
     return [LOOKUP.get(x, x) for x in tokens]
+
+
+def re_tokenize(text):
+    """
+    Returns a list of tokens from input text
+    Lowercase input, removing symbols and digts.
+    """
+    return re.findall(r"[A-Za-z]+", text.lower())
