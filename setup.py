367fc8a6e9bf000c4840d9b067542791d2f42397
Daniel Zheng <dzheng256@gmail.com>
2018-04-09 15:27:54 -0400

    change md to rst and add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='lextools',
+    version='0.1.0',
+    description='Package for processing text to measure lexical diversity.',
+    long_description=readme,
+    author='Daniel Zheng',
+    author_email='daniel.zheng@pitt.edu',
+    url='https://github.com/ELI-Data-Mining-Group/lextools',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
