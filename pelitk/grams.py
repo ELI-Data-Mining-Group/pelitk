8123471977df4d3986b528a9636ed963332e94c6
Daniel Zheng <dzheng256@gmail.com>
2018-04-29 22:13:16 -0400

    workng on grams

---
+++ pelitk/grams.py
@@ -0,0 +1,31 @@
+'''
+Python module
+
+'''
+
+
+
+__version__ = '0.1'
+__author__ = 'ELI Data Mining Group'
+
+
+
+def ngrams(tokens, n=1):
+    """ Takes list of tokens and returns list of ngrams of size n """
+
+    if n == 1:
+        return [[x] for x in tokens]
+    elif n < 1:
+        raise ValueError('Please specify an integer for n that is greater than 1.')
+    ngrams = [tokens[:n]]
+    for i in range(0, len(tokens)-n):
+        ngrams.append(ngrams[i][1:] + [tokens[i+n]])
+    return ngrams
+
+def skip_grams(tokens, n=2, skip=1):
+    if skip >= n:
+        raise ValueError('Please specify an integer for n that is greater than the one for skip')
+    n_grams = ngrams(tokens, n+skip)
+    # start with ngrams, then remove successively for skip grams
+    for gram in n_grams:
+        # wait.. is this how skip grams should work?
