6d97b0fee088443c8d87b17ed48bc36654b16961
Daniel Zheng <dzheng256@gmail.com>
2020-05-24 13:47:00 -0700

    yapf

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import sys
 from os import path
 from setuptools import setup, find_packages
 
-
 here = path.abspath(path.dirname(__file__))
 
 with open('README.md') as f:
@@ -13,20 +12,18 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-setup(
-    name='pelitk',
-    version='0.1.4',
-    description='Package for processing text, especially texts in PELIC',
-    long_description=readme,
-    authors='Daniel Zheng, Na-Rae Han, Ben Naismith',
-    author_email='naraehan@pitt.edu',
-    url='https://github.com/ELI-Data-Mining-Group/pelitk',
-    license=license,
-    install_requires=['nltk', 'scipy', 'numpy'],
-    packages=find_packages(exclude=('tests', 'docs')),
-    package_data={'pelitk': ['data/wordlists/*.txt', 'data/*.pkl']},
-    include_package_data=True
-)
+setup(name='pelitk',
+      version='0.1.4',
+      description='Package for processing text, especially texts in PELIC',
+      long_description=readme,
+      authors='Daniel Zheng, Na-Rae Han, Ben Naismith',
+      author_email='naraehan@pitt.edu',
+      url='https://github.com/ELI-Data-Mining-Group/pelitk',
+      license=license,
+      install_requires=['nltk', 'scipy', 'numpy'],
+      packages=find_packages(exclude=('tests', 'docs')),
+      package_data={'pelitk': ['data/wordlists/*.txt', 'data/*.pkl']},
+      include_package_data=True)
 
 if 'install' in sys.argv or 'develop' in sys.argv:
     import nltk
